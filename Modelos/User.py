e12e2ec7f21746946c630c0ec29b3adcdb98fba4
jebuitrago79 <jebuitrago79@ucatolica.edu.co>
2025-09-18 19:25:41 -0500

    Se crea la clase User.py

---
+++ Modelos/User.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from typing import Optional
+from enum import Enum
+from sqlalchemy import Column, String
+from sqlmodel import SQLModel, Field
+
+class EstadoCuenta(str, Enum):
+    activo = "activo"
+    bloqueado = "bloqueado"
+
+class UsuarioBase(SQLModel):
+    nombre: str
+    email: str
+    password: str
+    estado_cuenta: EstadoCuenta = EstadoCuenta.activo
+
+class Usuario(UsuarioBase, table=True):
+    __tablename__ = "usuarios"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    fecha_registro: datetime = Field(default_factory=datetime.utcnow)
+
+    tipo: str = Field(sa_column=Column(String, nullable=False, index=True))
+
+    __mapper_args__ = {
+        "polymorphic_on": "tipo",
+        "polymorphic_identity": "usuario",
+    }
