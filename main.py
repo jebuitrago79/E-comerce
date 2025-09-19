6fb90cdd78628151ac01bafb32912d684c508a91
juan <jearroyo27@ucatolica.edu.co>
2025-09-18 21:58:05 -0500

    Admin completado

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from db.init_db import create_db_and_tables
-from Routers import vendedores
+from Routers import vendedores, Administradores
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -10,7 +10,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="API E-comerce", lifespan=lifespan)
 app.include_router(vendedores.router)
-
+app.include_router(Administradores.router)
 @app.get("/")
 def root():
     return {"ok": True}
