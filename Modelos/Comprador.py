89fbe64581086d90cd0e013ec536300fb40c99d1
jebuitrago79 <jebuitrago79@ucatolica.edu.co>
2025-09-18 20:47:58 -0500

    Se modifica comprador

--- Modelos/Comprador.py
+++ Modelos/Comprador.py
@@ -1,11 +1,16 @@
 from typing import Optional
 from sqlmodel import Field
+from sqlalchemy import UniqueConstraint
 from .User import Usuario
 
 class Comprador(Usuario, table=True):
     __tablename__ = "compradores"
+    __table_args__ = (
+        UniqueConstraint("id_comprador", name="uq_compradores_id_comprador"),
+    )
 
     id: Optional[int] = Field(default=None, primary_key=True, foreign_key="usuarios.id")
-    id_comprador: Optional[int] = None
+
+    id_comprador: int = Field(index=True)
 
     __mapper_args__ = {"polymorphic_identity": "comprador"}
