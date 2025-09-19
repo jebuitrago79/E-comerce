f60e14bc613c4039e4ec9a355ffc5909b439141d
jebuitrago79 <jebuitrago79@ucatolica.edu.co>
2025-09-18 21:23:24 -0500

    Nueva clase usuario comun que hace herencia a las demas

---
+++ Modelos/common.py
@@ -0,0 +1,13 @@
+# Modelos/common.py
+from enum import Enum
+from sqlmodel import SQLModel
+
+class EstadoCuenta(str, Enum):
+    activo = "activo"
+    bloqueado = "bloqueado"
+
+class UserFields(SQLModel):
+    nombre: str
+    email: str
+    password: str
+    estado_cuenta: EstadoCuenta = EstadoCuenta.activo
