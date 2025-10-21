1c664baffb195e01fe5cc816572d2c4db98b7181
julian <jebuitrago79@ucatolica.edu.co>
2025-10-20 21:10:28 -0500

    El init_db se encarga de crear las tablas de cada uno de los modelos

--- db/init_db.py
+++ db/init_db.py
@@ -1,5 +1,13 @@
+# db/init_db.py
 from sqlmodel import SQLModel
-from .engine import engine
+from db.engine import engine
+from Modelos import Administrador, Comprador, Usuario, Vendedor  # importa tus modelos
 
 def create_db_and_tables():
+    """
+    Crea todas las tablas definidas en los modelos SQLModel
+    dentro de la base de datos conectada (Supabase o SQLite).
+    """
+    print("🛠️ Creando tablas en la base de datos (si no existen)...")
     SQLModel.metadata.create_all(engine)
+    print("✅ Tablas creadas correctamente.")
