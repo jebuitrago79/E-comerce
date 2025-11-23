fd58afea957bdb77a3ad6012515b1bf02114ee1b
jebuitrago79 <jebuitrago79@ucatolica.edu.co>
2025-11-22 21:22:14 -0500

    cambios en compradores y providers

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -2,17 +2,14 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
-import { CartProvider } from "@/app/context/CartContext";
- // 👈 IMPORTANTE
+import { CartProvider } from "./context/CartContext";
 
 export default function Providers({ children }: { children: ReactNode }) {
   const [client] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={client}>
-      <CartProvider>   {/* 👈 Aquí activamos el carrito global */}
-        {children}
-      </CartProvider>
+      <CartProvider>{children}</CartProvider>
     </QueryClientProvider>
   );
 }
--- frontend/lib/compradores.ts
+++ frontend/lib/compradores.ts
@@ -14,8 +14,6 @@ export interface Comprador {
 
 
 
-
-
 export type CompradorLoginResponse = {
   id: number;
   id_comprador?: number;
